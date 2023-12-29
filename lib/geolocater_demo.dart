cbebea910e01657674cfb4b7c8b05fd96318e1dc
22shubh22 <22shubh22@gmail.com>
2023-12-29 12:56:48 +0530

    geolocater successfull usecase

---
+++ lib/geolocater_demo.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MyGeolocationWidget extends StatefulWidget {
+  @override
+  _MyGeolocationWidgetState createState() => _MyGeolocationWidgetState();
+}
+
+
+class _MyGeolocationWidgetState extends State<MyGeolocationWidget> {
+  late Position _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check for permissions
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Handle the case where location services are disabled
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Handle the case where permission is denied
+        return;
+      }
+    }
+
+    try {
+      _currentPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      setState(() {});
+    } catch (e) {
+      // Handle any errors that occur
+      print(e);
+    }
+  }
+
+  // Future<void> _getLocation() async {
+  //   var _locationMessage; 
+  //   Position position;
+  //   try {
+  //     position = await Geolocator.getCurrentPosition(
+  //         desiredAccuracy: LocationAccuracy.high);
+  //   } catch (e) {
+  //     position = null
+  //   }
+  //   if (position) {
+  //     setState(() {
+  //       _locationMessage =
+  //           'Latitude: ${position.latitude}, Longitude: ${position.longitude}';
+  //     });
+  //   } else {
+  //     setState(() {
+  //       _locationMessage = 'Unable to retrieve location';
+  //     });
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Geolocation Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_currentPosition != null)
+              Text('Latitude: ${_currentPosition.latitude}'),
+            if (_currentPosition != null)
+              Text('Longitude: ${_currentPosition.longitude}'),
+            ElevatedButton(
+              onPressed: _determinePosition,
+              child: const Text('Get Location'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'geolocater_demo.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,18 +22,65 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('SMVD Track sahayak'),
+        title: const Text('SMVD Track sahayak'),
       ),
-      body: Center(
-        child: Text(
-          '18.2 km to bhawan',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-          ),
-          textAlign: TextAlign.center,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text('Home Page'),
+              onTap: () {
+                _navigateToPage(0);
+              },
+            ),
+            ListTile(
+              title: const Text('Geolocater Demo'),
+              onTap: () {
+                _navigateToPage(1);
+              },
+            ),
+          ],
         ),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            '18.2 km to bhawan',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            '18.2 km to bhawan',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              _navigateToGeolocatorPage(context);
+            },
+            child: const Text('Go to geo locater'),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _navigateToPage(int pageIndex) {
+    Navigator.pop(context); // Close the drawer after navigation
+  }
+
+  void _navigateToGeolocatorPage(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) =>
+            MyGeolocationWidget())); // Close the drawer after navigation
   }
 }
